8679e1a136bc798141fd0a223c14fbe7f2d6773d
deniznida <deniz@flatironschool.com>
2016-03-08 17:06:25 -0500

    passing tests

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Dee';
+
+  $timeout(function(){
+    $scope.name = "Deniz"
+  }, 5000);
 }
 
 angular
